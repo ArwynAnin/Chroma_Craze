771122b7d8e22b03c4702562438cd7771cbdf48a
ArwynAnin <arwyn.anin@ciit.edu.ph>
2023-11-12 14:16:09 +0800

    pause scene for both and audio

---
+++ Assets/Scripts/audioplayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioplayer : MonoBehaviour
+{
+    public AudioSource audioplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioplay.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
